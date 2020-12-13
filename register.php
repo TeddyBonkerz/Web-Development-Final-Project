d37a38aac2a13d1259b0ce14223d8715c6b81ffd
TeddyBonkerz <radzlaif@gmail.com>
2020-12-12 20:55:31 -0500

    more changes and shit

--- register.php
+++ register.php
@@ -1,71 +1,71 @@
 <?php
 require_once "./config.php";
- 
+
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate username
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter a username.";
-    } else{
+    } else {
         $sql = "SELECT id FROM users WHERE username = ?";
 
-        if($stmt = mysqli_prepare($link, $sql)){
+        if ($stmt = mysqli_prepare($link, $sql)) {
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             $param_username = trim($_POST["username"]);
 
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 1){
+
+                if (mysqli_stmt_num_rows($stmt) == 1) {
                     $username_err = "This username is already taken.";
-                } else{
+                } else {
                     $username = trim($_POST["username"]);
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
             mysqli_stmt_close($stmt);
         }
     }
-    
+
     // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
         $password_err = "Password must have atleast 6 characters.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm password.";
+    } else {
         $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
+        if (empty($password_err) && ($password != $confirm_password)) {
             $confirm_password_err = "Password did not match.";
         }
     }
-    
+
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+
         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
 
             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-            
+
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 header("location: ./login.php");
-            } else{
+            } else {
                 echo "Something went wrong. Please try again later.";
             }
             mysqli_stmt_close($stmt);
@@ -74,18 +74,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     mysqli_close($link);
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
-    
+
     <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
+        body {
+            font: 14px sans-serif;
+        }
+
+        .wrapper {
+            width: 350px;
+            padding: 20px;
+            margin: 0 auto;
+        }
     </style>
 </head>
+
 <body>
     <div class="wrapper">
         <h2>Sign Up</h2>
@@ -94,17 +103,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo "<br>"; echo $username_err; ?></span>
-            </div>    
+                <span class="help-block"><?php echo "<br>";
+                                            echo $username_err; ?></span>
+            </div>
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo "<br>"; echo $password_err; ?></span>
+                <span class="help-block"><?php echo "<br>";
+                                            echo $password_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo "<br>"; echo $confirm_password_err; ?></span>
+                <span class="help-block"><?php echo "<br>";
+                                            echo $confirm_password_err; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
@@ -112,6 +124,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <p>Already have an account? <a href="./login.php">Login here</a>.</p>
         </form>
-    </div>    
+    </div>
 </body>
+
 </html>
--- welcome.php
+++ welcome.php
@@ -18,20 +18,31 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
          font: 14px sans-serif;
          text-align: center;
       }
+      a:hover{
+         background: yellow;
+      }
+      .content{
+         margin: auto
+      }
+      .task-list{
+         display: inline-block; text-align: left;
+      }
    </style>
 </head>
 
 <body>
    <h1> Artist and Album Database</h1>
    <div class="page-header">
-      <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+      <h1>Welcome, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></h1>
    </div>
-   <h3> Which task would you like to perform ? </h3>
-   <ul>
-      <li><a href="./addArtistForm.php">Add an artist</a></li>
-      <li><a href="./addAlbumForm.php">Add and Album</a></li>
-      <li><a href="./searchDataBase.php">Search for Artist or Album</a></li>
-   </ul>
+   <div class="content">
+      <h3> Which task would you like to perform ? </h3>
+      <ul class="task-list">
+         <li><a href="./addArtistForm.php">Add an artist</a></li>
+         <li><a href="./addAlbumForm.php">Add and Album</a></li>
+         <li><a href="./searchDataBase.php">Search for Artist or Album</a></li>
+      </ul>
+   </div>
    <p>
       <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
    </p>
