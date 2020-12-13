ca06b722da1e0a5a17e71a934869c7053a52fb78
TeddyBonkerz <radzlaif@gmail.com>
2020-12-13 00:10:17 -0500

    Added basic error checking for addArtistForm

--- addArtistForm.php
+++ addArtistForm.php
@@ -1,5 +1,22 @@
 <?php
+require_once "./config.php";
 
+
+$artistname = mysqli_real_escape_string($link, $_REQUEST['artist_name']);
+$artistname_err = "";
+
+if (empty(trim($_POST["artist_name"]))) {
+    $artistname_err = "Please enter an Artist Name.";
+} else {
+    $sql = "INSERT INTO Artists (artistName) VALUES ('$artistname')";
+
+    if (mysqli_query($link, $sql)) {
+        echo "Artist added successfully.";
+    } else {
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    }
+    mysqli_close($link);
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,10 +43,10 @@
     <div class="wrapper">
         <h1>Add an Artist</h1>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($artistiname_err)) ? 'has-error' : ''; ?>">
+            <div class="form-group <?php echo (!empty($artistname_err)) ? 'has-error' : ''; ?>">
                 <label>Artist Name</label>
-                <input type="text" name="artistname" class="form-control" value="<?php echo $artistname; ?>">
-                <span class="help-block"><?php echo "<br>"; echo $artistiname_err; ?></span>
+                <input type="text" name="artist_name" id="artistname" value="<?php echo $artistname; ?>">
+                <span class="help-block"><?php echo "<br>" + $artistname_err; ?></span>
             </div>
 
             <input type="submit" class="submit-button" value="Submit">
